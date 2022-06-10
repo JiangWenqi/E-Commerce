71af6c36bf0edbc72e004a51ce9bc105355c8b8c
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-06-11 00:41:21 +0200

    init a spring project

---
+++ e-commerce/src/main/java/info/jiangwenqi/e_commerce/ECommerceApplication.java
@@ -0,0 +1,16 @@
+package info.jiangwenqi.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wenqi
+ */
+@SpringBootApplication
+public class ECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
+
+}
